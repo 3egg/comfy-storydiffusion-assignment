6e15f078a3fc6bc09ac3954c8e5ed5b90f1bfca7
ltian@beyond.ai <627096931@qq.com>
2025-05-15 16:31:17 +0800

    fix: remove useless code

--- worker/src/main/java/com/example/worker/WorkerApplication.java
+++ worker/src/main/java/com/example/worker/WorkerApplication.java
@@ -1,32 +1,13 @@
 package com.example.worker;
 
-import com.example.worker.service.CustomWebSocketClientV1;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.net.URI;
 
 @SpringBootApplication
 public class WorkerApplication{
 
-//    private final String clientId;
-//
-//    public WorkerApplication(@Value("${clientId}") String clientId) {
-//        this.clientId = clientId;
-//    }
-
     public static void main(String[] args) {
         SpringApplication.run(WorkerApplication.class, args);
     }
-
-    /*@Override
-    public void run(String... args) {
-        CustomWebSocketClientV1 customWebSocketClientV1 = new CustomWebSocketClientV1(
-                URI.create("ws://127.0.0.1:8000/ws?clientId=" + clientId)
-        );
-        customWebSocketClientV1.connect();
-    }*/
 
 }
